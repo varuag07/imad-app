6634a713d65a9c5c93bced13f3f76813d9479e1e
varuag07 <30632838+varuag07@users.noreply.github.com>
2017-08-03 21:48:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function (req, res) {
-  res.send('Article 1 Page was requested.');
+app.get('/ui/article-one.html', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send('Article 2 Page was requested.');
