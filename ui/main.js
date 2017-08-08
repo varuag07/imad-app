bb8d29ff9a060f54411ee19f36d62b51efa49b34
varuag07 <30632838+varuag07@users.noreply.github.com>
2017-08-08 22:17:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
 console.log('Loaded!');
+var button = document.getElementById('counter');
+
+button.onClick = function() {
+    //Create request to Counter endpoint
+    var request = new XMLHttpRequest();
+    
+    //Capture the response
+    request.onreadystatechange = function() {
+      if(request.readyState === XMLHttpRequest.Done)
+      {
+          if(request.status === 200)
+          {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+          }
+      }
+    };
+    //Make the Http Request.
+    request.open('GET','http://varuag07.imad.hasura-app.io/counter',true);
+    result.send(null);
+};
