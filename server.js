d785ed976b2d011c039c9f910e359ca34032a73e
varuag07 <30632838+varuag07@users.noreply.github.com>
2017-08-20 14:13:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ app.post('/login', function(req,res){
     var username = req.body.username;
     var password = req.body.password;
     
-    pool.query('SELECT * FROM "UserLogin" WHERE \"UserName\"=$1', [username], function (err, result){
+    pool.query('SELECT * FROM "UserLogin" WHERE "UserName"= $1', [username], function (err, result){
         
         if(err)
         {
