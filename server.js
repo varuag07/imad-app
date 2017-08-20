8ae57fe6c8cf260b4f693f36eb89d566cbca4138
varuag07 <30632838+varuag07@users.noreply.github.com>
2017-08-20 11:30:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var crypto = require('crypto');
 var Pool = require('pg').Pool;
 var config = {
     user : 'varuag07',
@@ -15,6 +16,19 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+app.get('/hash/input', function (req,res) {
+    var hashedString = hash(req.params.input, 'this is a random string');
+    res.send(hashedString);
+});
+
+function hash(input)
+{
+    //Create a Hash.
+    var hashed = crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+    return hashed.toString('hex');
+}
+
 /*app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
