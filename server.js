4172e46af9387af872e6abdb124da3ba06b41d08
varuag07 <30632838+varuag07@users.noreply.github.com>
2017-08-20 18:58:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -99,7 +99,7 @@ app.post('/login', function(req,res){
 });
 
 app.get('/check-login', function(req, res){
-    console.log(req.session.auth.userId.toString());
+    console.log(req.session.auth.userId);
     if(req.session && req.session.auth && req.session.auth.userId)
     {
         res.send("You are logged in: " + req.session.auth.userId.toString());
